b57c27de5b69f6649f75ddd0754374ea6c390131
andreiusq <mrandyadv@gmail.com>
2021-08-15 21:29:37 +0300

    Gateaway improvment

--- profil.php
+++ profil.php
@@ -44,7 +44,7 @@ foreach($users as $user)
                 <div class="content-header-left col-md-9 col-12 mb-2">
                     <div class="row breadcrumbs-top">
                         <div class="col-12">
-                            <h2 class="content-header-title float-left mb-0">Profile</h2>
+                            <h2 class="content-header-title float-left mb-0">Profil</h2>
                             <div class="breadcrumb-wrapper">
                                 <ol class="breadcrumb">
                                     <li class="breadcrumb-item"><a href="index.html">Home</a>
