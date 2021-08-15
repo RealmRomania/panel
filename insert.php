50688399a02d40f45ad3a8fb3626f20585010a74
andreiusq <mrandyadv@gmail.com>
2021-08-15 23:05:19 +0300

    Notifications Changes

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 if(isset($_POST["subject"]))
 {
 include("db.php");
-$subject = mysqli_real_escape_string($con, $_POST["subject"]);
+$subject = mysqli_real_escape_string($con, $_POST["ssubject"]);
 $comment = mysqli_real_escape_string($con, $_POST["text"]);
 $query = "INSERT INTO notificari(subject, text) VALUES ('$subject', '$text')";
 mysqli_query($con, $query);
