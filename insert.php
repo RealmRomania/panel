bfbe8c98e3cdf2d4bab2ecd945f91e9ea23a1333
andreiusq <mrandyadv@gmail.com>
2021-08-15 23:05:41 +0300

    Maintenance mode changes

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 if(isset($_POST["subject"]))
 {
 include("db.php");
-$subject = mysqli_real_escape_string($con, $_POST["ssubject"]);
+$subject = mysqli_real_escape_string($con, $_POST["psubject"]);
 $comment = mysqli_real_escape_string($con, $_POST["text"]);
 $query = "INSERT INTO notificari(subject, text) VALUES ('$subject', '$text')";
 mysqli_query($con, $query);
